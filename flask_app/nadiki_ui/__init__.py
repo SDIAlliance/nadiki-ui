3c03da5fb81e29a9d5c4784435d8bf5282ea2d26
Daniel Boesswetter <daniel@daniel-boesswetter.de>
2025-04-25 17:57:20 +0200

    delete the snapshot bucket when deleting the snapshot

--- flask_app/nadiki_ui/__init__.py
+++ flask_app/nadiki_ui/__init__.py
@@ -14,12 +14,12 @@ from nadiki_ui.influxdb import influxdb_client
 app = Flask(__name__)
 
 sqs = boto3.client('sqs')
+buckets_api = influxdb_client.buckets_api()
 
 @app.route("/")
 def homepage():
     with engine.connect() as conn:
         result = conn.execute(select(snapshots))
-    buckets_api = influxdb_client.buckets_api()
     bucket_names = [x["name"] for x in buckets_api.find_buckets().to_dict()["buckets"]]
     
     return render_template("index.html", snapshots=result, bucket_names=bucket_names, delete_url=url_for("delete_snapshot"), insert_url=url_for("insert_snapshot"))
@@ -27,6 +27,8 @@ def homepage():
 @app.route("/delete")
 def delete_snapshot():
     with engine.connect() as conn:
+        bucket = buckets_api.find_bucket_by_name(f"SNAPSHOT-{request.args.get('id')}")
+        buckets_api.delete_bucket(bucket.id)
         conn.execute(delete(snapshots).where(snapshots.c.id == request.args.get('id')))
         conn.commit()
     return redirect(url_for("homepage"))
